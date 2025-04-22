3e998395993306f4bba3a65a9a607fadcb7e461f
dinakajoy <odinakajoy909@gmail.com>
2025-04-22 20:38:22 +0100

    updated

--- lib/techtomes/nodejs-mastery/nodejs-mastery-routes-config.ts
+++ lib/techtomes/nodejs-mastery/nodejs-mastery-routes-config.ts
@@ -11,38 +11,16 @@ export const NODEJS_ROUTES: EachRoute[] = [
       { title: "Introduction to Node.js", href: "/introduction-to-nodejs" },
       { title: "More on Node.js Ecosystem", href: "/nodejs-ecosystem" },
     ],
-    // noLink: false,
-    // items: [
-    //   { title: "What is Node.js?", href: "/what-is-nodejs" },
-    //   { title: "History and Evolution of Node.js", href: "/history-of-nodejs" },
-    //   {
-    //     title: "Why Use Node.js?",
-    //     href: "/why-nodejs",
-    //     items: [
-    //       { title: "Benefits of Node.js", href: "/benefits-of-nodejs" },
-    //       { title: "Use Cases & Industry Adoption", href: "/use-of-nodejs" },
-    //     ],
-    //   },
-    //   {
-    //     title: "Understanding the Node.js Ecosystem",
-    //     href: "/understanding-nodejs",
-    //     items: [
-    //       { title: "Package Managers", href: "/package-managers" },
-    //       { title: "The Role of V8 JavaScript Engine", href: "/role-of-v8-engine" },
-    //       { title: "CommonJS vs ECMAScript Modules", href: "/commonjs-vs-esmodules" },
-    //     ],
-    //   },
-    // ],
   },
-  // {
-  //   title: "Setting Up Node.js",
-  //   href: "/server-actions",
-  //   noLink: true,
-  //   items: [
-  //     { title: "getSession", href: "/getSession" },
-  //     { title: "getToken", href: "/getToken" },
-  //   ],
-  // },
+  {
+    title: "Node.js Fundamentals",
+    href: "/nodejs-fundamentals",
+    noLink: true,
+    items: [
+      { title: "JavaScript vs Node.js", href: "/javascript-vs-nodejs" },
+      { title: "Backend versus Frontend Development", href: "/backend-vs-frontend-development" },
+    ],
+  },
 ];
 
 function getRecurrsiveAllLinks(node: EachRoute) {
