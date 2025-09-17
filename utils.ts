73fa3474ec723651451f8684807f5104f977df67
dinakajoy <dinakajoy@gmail.com>
2025-09-17 23:02:22 +0100

    Updated portfolio

--- utils.ts
+++ utils.ts
@@ -1,7 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { NODEJS_ROUTES } from "./techtomes/nodejs-mastery/nodejs-mastery-routes-config";
-import { EachRoute } from "@/interfaces/routes.interface";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
